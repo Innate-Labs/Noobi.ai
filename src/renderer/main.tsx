e68228843548816b8864dd1dc17638e56e564f8d
PLA-yi <124276371+PLA-yi@users.noreply.github.com>
2026-08-31 17:54:27 +0800

    style: 放大品牌字标并使用像素字体

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import '@fontsource/press-start-2p/latin-400.css';
 import { App } from './App';
 import './styles.css';
 
